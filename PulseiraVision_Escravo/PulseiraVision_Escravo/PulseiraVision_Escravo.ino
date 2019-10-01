b5388482eae505f9f49493c4f15c26975d001978
Anderson <andcsoza@hotmail.com>
2019-10-01 11:20:38 -0300

    Primeiro teste Escravo

--- PulseiraVision_Escravo/PulseiraVision_Escravo/PulseiraVision_Escravo.ino
+++ PulseiraVision_Escravo/PulseiraVision_Escravo/PulseiraVision_Escravo.ino
@@ -2,25 +2,48 @@
 
 const int rxpin = 2;
 const int txpin = 3;
+
+int botao_liga = 9;
+int botao_desliga = 8;
+int led_liga = 7;
+int led_desliga = 6;
+int led_recebe = 5;
+
+int estado_botao_liga = 0;
+int estado_botao_desliga = 0;
+
 SoftwareSerial bluetooth(rxpin, txpin);
 
-void setup(){
+void setup() {
+  pinMode(botao_liga, INPUT);
+  pinMode(botao_desliga, INPUT);
+  pinMode(led_liga, OUTPUT);
+  pinMode(led_desliga, OUTPUT);
+  pinMode(led_recebe, OUTPUT);
   Serial.begin(9600);
   bluetooth.begin(9600); // initialize the software serial port
   //bluetooth.begin(38400);
   Serial.println("Serial ready");
   bluetooth.println("Bluetooth ready");
+
+  bluetooth.print("AT+ROLE0");
+  bluetooth.print("AT+IMME0");
 }
 
-void loop(){
-  if (bluetooth.available()){
+void loop() {
+  digitalWrite(led_recebe, LOW);
+  while(bluetooth.available()) {
+    digitalWrite(led_recebe, HIGH);
     char c = (char)bluetooth.read();
-    Serial.write(c);
-    //bluetooth.print("10");
-  }
-  if (Serial.available()) {
-    char c = (char)Serial.read();
-    bluetooth.write(c);
-   // bluetooth.print("10");
+    estado_botao_liga = digitalRead(botao_liga);
+    if (estado_botao_liga == HIGH) {
+      digitalWrite(led_liga, HIGH);
+    }
+    else if (estado_botao_liga == LOW) {
+      digitalWrite(led_liga, LOW);
+    }
+        char c = (char)bluetooth.read();
+        Serial.write(c);
+    bluetooth.print("10");
   }
 }
