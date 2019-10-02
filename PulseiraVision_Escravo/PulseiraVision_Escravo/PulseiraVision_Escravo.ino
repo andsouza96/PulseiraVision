0aaacf5bff02d2d0cacaa0112fdbf1738b000784
Anderson <andcsoza@hotmail.com>
2019-10-02 10:04:39 -0300

    armazenando char em vetores

--- PulseiraVision_Escravo/PulseiraVision_Escravo/PulseiraVision_Escravo.ino
+++ PulseiraVision_Escravo/PulseiraVision_Escravo/PulseiraVision_Escravo.ino
@@ -21,20 +21,34 @@ void setup() {
   pinMode(led_desliga, OUTPUT);
   pinMode(led_recebe, OUTPUT);
   Serial.begin(9600);
-  bluetooth.begin(9600); // initialize the software serial port
-  //bluetooth.begin(38400);
+  bluetooth.begin(9600);
   Serial.println("Serial ready");
-  bluetooth.println("Bluetooth ready");
-
   bluetooth.print("AT+ROLE0");
   bluetooth.print("AT+IMME0");
 }
 
 void loop() {
   digitalWrite(led_recebe, LOW);
-  while(bluetooth.available()) {
-    digitalWrite(led_recebe, HIGH);
-    char c = (char)bluetooth.read();
+  while (bluetooth.available()) {
+
+    //  char c = (char)bluetooth.read();
+    String teste[10];
+    int i = 1;
+    for (i; i < 10; i++) {
+      
+    //  Serial.println(sizeof(bluetooth.available())); //tamanho do byte recebido (cada char)
+      digitalWrite(led_recebe, HIGH);
+      delay(1000);
+      char c = bluetooth.read();
+      bluetooth.print(c);
+      Serial.println(c);
+     
+      teste[i] = teste[i] + c;    //armazenando cada char em um vetor
+    }
+    for (int a =0; a < 10; a++) {
+      Serial.print(teste[a]); // imprime na tela o vetor
+    }
+
     estado_botao_liga = digitalRead(botao_liga);
     if (estado_botao_liga == HIGH) {
       digitalWrite(led_liga, HIGH);
@@ -42,8 +56,8 @@ void loop() {
     else if (estado_botao_liga == LOW) {
       digitalWrite(led_liga, LOW);
     }
-        char c = (char)bluetooth.read();
-        Serial.write(c);
+    //        char c = (char)bluetooth.read();
+    //      Serial.write(c);
     bluetooth.print("10");
   }
 }
